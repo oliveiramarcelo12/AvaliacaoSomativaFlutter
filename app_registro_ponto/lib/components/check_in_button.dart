314c79594b586053468b7a12017ed08c91de173b
MateusOliveiraT25 <oliveiramateus1222@gmail.com>
2024-11-05 19:27:31 -0300

    05/11/24

--- app_registro_ponto/lib/components/check_in_button.dart
+++ app_registro_ponto/lib/components/check_in_button.dart
@@ -1,17 +1,23 @@
-// lib/components/check_in_button.dart
 import 'package:flutter/material.dart';
 
 class CheckInButton extends StatelessWidget {
   final bool isLoading;
   final VoidCallback onPressed;
+  final String text; // Adicionando o texto dinâmico
 
-  CheckInButton({required this.isLoading, required this.onPressed});
+  CheckInButton({required this.isLoading, required this.onPressed, required this.text});
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
       onPressed: isLoading ? null : onPressed, // Desabilita o botão se isLoading for true
-      child: isLoading ? CircularProgressIndicator() : Text('Registrar Ponto'),
+      child: isLoading
+          ? SizedBox(
+              width: 24, // Tamanho fixo para o CircularProgressIndicator
+              height: 24,
+              child: CircularProgressIndicator(strokeWidth: 2),
+            )
+          : Text(text), // Mostra o texto com base na variável 'text'
     );
   }
 }
--- app_registro_ponto/lib/screens/check_in_screen.dart
+++ app_registro_ponto/lib/screens/check_in_screen.dart
@@ -12,74 +12,75 @@ class CheckInScreen extends StatefulWidget {
 }
 
 class _CheckInScreenState extends State<CheckInScreen> {
-  bool _isCheckingIn = false;
+  bool _isLoading = false;
   String _statusMessage = 'Aguardando ação do usuário...';
+  bool _isCheckingIn = true; // Flag para indicar se é registro de entrada
 
-  Future<void> _handleCheckIn() async {
+  Future<void> _handleCheckInOut() async {
     setState(() {
-      _isCheckingIn = true;
-      _statusMessage = 'Verificando localização...';
+      _isLoading = true;
+      _statusMessage = _isCheckingIn ? 'Verificando localização para entrada...' : 'Verificando localização para saída...';
     });
 
-    try {
-      // Solicitar permissão de localização ao usuário
-      LocationPermission permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.denied || permission == LocationPermission.deniedForever) {
-        setState(() {
-          _statusMessage = 'Permissão de localização negada.';
-          _isCheckingIn = false;
-        });
-        return;
-      }
-
-      // Obter a localização do usuário
-      Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-      double userLatitude = position.latitude;
-      double userLongitude = position.longitude;
-
-      // Checar se está dentro do raio permitido (definido no LocationService)
-      bool isWithinRange = await LocationService.checkUserLocation(userLatitude, userLongitude);
-
-      if (isWithinRange) {
-        setState(() {
-          _statusMessage = 'Localização confirmada. Registrando ponto...';
-        });
-
-        // Obtenha a referência do Firestore e o usuário atual
-        User? user = FirebaseAuth.instance.currentUser;
-        FirebaseFirestore firestore = FirebaseFirestore.instance;
-
-        // Obtenha a data e hora atuais
-        DateTime now = DateTime.now();
-        String formattedDate = DateFormat('dd/MM/yyyy').format(now);
-        String formattedTime = DateFormat('HH:mm:ss').format(now);
-
-        // Salve o registro no Firestore
-        await firestore.collection('check_ins').add({
-          'userId': user?.uid,
-          'date': formattedDate,
-          'time': formattedTime,
-          'location': 'Dentro da área permitida',
-        });
-
-        // Mensagem de sucesso com data e hora
-        setState(() {
-          _statusMessage = 'Ponto registrado com sucesso em $formattedDate às $formattedTime!';
-        });
-      } else {
-        setState(() {
-          _statusMessage = 'Fora da área permitida para registro de ponto.';
-        });
-      }
-    } catch (e) {
+    // Solicitar permissão de localização ao usuário
+    LocationPermission permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied || permission == LocationPermission.deniedForever) {
       setState(() {
-        _statusMessage = 'Erro ao registrar o ponto: $e';
+        _statusMessage = 'Permissão de localização negada.';
+        _isLoading = false;
       });
-    } finally {
+      return;
+    }
+
+    // Obter a localização do usuário
+    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    double userLatitude = position.latitude;
+    double userLongitude = position.longitude;
+
+    // Checar se está dentro do raio permitido
+    bool isWithinRange = await LocationService.checkUserLocation(userLatitude, userLongitude);
+
+    if (isWithinRange) {
       setState(() {
-        _isCheckingIn = false;
+        _statusMessage = 'Localização confirmada. Registrando ${_isCheckingIn ? 'entrada' : 'saída'}...';
+      });
+
+      // Obtenha a referência do Firestore e o usuário atual
+      User? user = FirebaseAuth.instance.currentUser;
+      FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+      // Obtenha a data e hora atuais
+      DateTime now = DateTime.now();
+      String formattedDate = DateFormat('dd/MM/yyyy').format(now);
+      String formattedTime = DateFormat('HH:mm:ss').format(now);
+
+      // Salve o registro no Firestore
+      await firestore.collection('check_ins').add({
+        'userId': user?.uid,
+        'date': formattedDate,
+        'time': formattedTime,
+        'type': _isCheckingIn ? 'entrada' : 'saida', // Tipo de registro
+        'location': 'Dentro da área permitida',
+      });
+
+      setState(() {
+        _statusMessage = '${_isCheckingIn ? 'Entrada' : 'Saída'} registrada com sucesso em $formattedDate às $formattedTime!';
+      });
+    } else {
+      setState(() {
+        _statusMessage = 'Fora da área permitida para registro de ${_isCheckingIn ? 'entrada' : 'saída'}.';
       });
     }
+
+    setState(() {
+      _isLoading = false;
+    });
+  }
+
+  void _toggleCheckInOut() {
+    setState(() {
+      _isCheckingIn = !_isCheckingIn; // Alterna entre entrada e saída
+    });
   }
 
   @override
@@ -100,8 +101,26 @@ class _CheckInScreenState extends State<CheckInScreen> {
             ),
             SizedBox(height: 20),
             CheckInButton(
-              isLoading: _isCheckingIn,
-              onPressed: _handleCheckIn,
+              isLoading: _isLoading,
+              onPressed: _handleCheckInOut,
+              text: _isCheckingIn ? 'Registrar Entrada' : 'Registrar Saída', // Texto do botão
+            ),
+            SizedBox(height: 20),
+            // Mostra o botão de saída somente quando for registrar saída
+            if (!_isCheckingIn) 
+              CheckInButton(
+                isLoading: _isLoading,
+                onPressed: _handleCheckInOut,
+                text: 'Registrar Saída', // Texto do botão de saída
+              ),
+            // Mostra o botão para alternar entre entrada e saída
+            SizedBox(height: 20),
+            TextButton(
+              onPressed: _toggleCheckInOut,
+              child: Text(
+                _isCheckingIn ? 'Mudar para Saída' : 'Mudar para Entrada',
+                style: TextStyle(fontSize: 16),
+              ),
             ),
           ],
         ),
