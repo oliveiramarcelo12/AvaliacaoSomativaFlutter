08207498349e996dcd16d133bb25ef6a8d800e26
Mateus Oliveira <123783867+MateusOliveiraT25@users.noreply.github.com>
2024-11-13 14:40:43 -0300

    13/11/24

--- app_registro_ponto/lib/components/check_in_button.dart
+++ app_registro_ponto/lib/components/check_in_button.dart
@@ -1,23 +1,36 @@
 import 'package:flutter/material.dart';
 
+/// Botão personalizado para o registro de ponto.
+///
+/// `CheckInButton` exibe um botão que muda para um indicador de carregamento 
+/// quando uma operação assíncrona está em andamento. Recebe um texto dinâmico, 
+/// uma função `onPressed`, e cores para customização.
 class CheckInButton extends StatelessWidget {
-  final bool isLoading;
-  final VoidCallback onPressed;
-  final String text; // Adicionando o texto dinâmico
+  final bool isLoading;          // Indica se o botão está em estado de carregamento
+  final VoidCallback onPressed;  // Função que é chamada quando o botão é pressionado
+  final String text;             // Texto dinâmico exibido no botão
 
-  CheckInButton({required this.isLoading, required this.onPressed, required this.text, required Color buttonColor, required Color textColor});
+  // Construtor que inicializa as propriedades obrigatórias
+  CheckInButton({
+    required this.isLoading,
+    required this.onPressed,
+    required this.text,
+    required Color buttonColor,
+    required Color textColor,
+  });
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      onPressed: isLoading ? null : onPressed, // Desabilita o botão se isLoading for true
+      // O botão é desabilitado se `isLoading` for true para evitar cliques múltiplos
+      onPressed: isLoading ? null : onPressed,
       child: isLoading
           ? SizedBox(
-              width: 24, // Tamanho fixo para o CircularProgressIndicator
+              width: 24, // Largura fixa do CircularProgressIndicator para uniformidade
               height: 24,
               child: CircularProgressIndicator(strokeWidth: 2),
             )
-          : Text(text), // Mostra o texto com base na variável 'text'
+          : Text(text), // Exibe o texto dinâmico quando não está carregando
     );
   }
 }
