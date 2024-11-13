90f27f969b25b3ab5ba06a8264f0857948f90297
Mateus Oliveira <123783867+MateusOliveiraT25@users.noreply.github.com>
2024-11-13 14:39:08 -0300

    13/11/24

--- app_registro_ponto/lib/firebase_options.dart
+++ app_registro_ponto/lib/firebase_options.dart
@@ -1,18 +1,27 @@
-// firebase_options.dart
-
+// Importações necessárias para inicializar o Firebase no aplicativo Flutter
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
+/// Classe que armazena as configurações padrão do Firebase para o projeto.
+///
+/// Contém as informações necessárias para conectar o aplicativo ao projeto Firebase,
+/// como `apiKey`, `appId`, e outras credenciais que identificam o projeto.
 class DefaultFirebaseOptions {
+  
+  /// Método estático que retorna as configurações de Firebase para a plataforma atual.
+  ///
+  /// Aqui são definidas as credenciais do projeto Firebase, necessárias para autenticação,
+  /// armazenamento e outras integrações com o Firebase.
   static FirebaseOptions get currentPlatform {
+    // Configurações específicas do projeto Firebase
     return const FirebaseOptions(
-      apiKey: "AIzaSyDhcqrkLeDPOtZzxqSwOsl9EsWm7xRH6pc",
-      appId: "1:197911325551:android:06d24774ec7d85c196569a",
-      messagingSenderId: "197911325551",
-      projectId: "testefirebasenoite",
-      storageBucket: "testefirebasenoite.appspot.com",
-      authDomain: "testefirebasenoite.firebaseapp.com",
+      apiKey: "AIzaSyDhcqrkLeDPOtZzxqSwOsl9EsWm7xRH6pc",           // Chave de API do Firebase
+      appId: "1:197911325551:android:06d24774ec7d85c196569a",     // ID do aplicativo
+      messagingSenderId: "197911325551",                          // ID do remetente do Firebase Cloud Messaging
+      projectId: "testefirebasenoite",                            // ID do projeto no Firebase
+      storageBucket: "testefirebasenoite.appspot.com",            // URL do bucket de armazenamento do Firebase
+      authDomain: "testefirebasenoite.firebaseapp.com",           // Domínio de autenticação do Firebase
     );
   }
 }
