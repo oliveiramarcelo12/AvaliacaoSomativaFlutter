bc76b1df81aa3f80aebfc85fd254edb042fc9a45
Mateus Oliveira <123783867+MateusOliveiraT25@users.noreply.github.com>
2024-11-13 14:39:50 -0300

    13/11/24

--- app_registro_ponto/lib/widgets/loading_spinner.dart
+++ app_registro_ponto/lib/widgets/loading_spinner.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 
+/// Widget personalizado que exibe um indicador de carregamento centralizado.
+///
+/// `LoadingSpinner` é usado para mostrar ao usuário que uma operação assíncrona
+/// está em andamento, como uma requisição de rede ou carregamento de dados.
 class LoadingSpinner extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
+      // O CircularProgressIndicator é um widget nativo do Flutter que exibe um spinner animado.
       child: CircularProgressIndicator(),
     );
   }
